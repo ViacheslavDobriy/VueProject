8cf5f52eef3b7d79bb3d793fba254be5703b6510
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-07-31 22:05:36 +0800

    Added component tags

---
+++ components/tags.js
@@ -0,0 +1,253 @@
+const tags = {
+    data() {
+        return {
+            tagsData: [
+                {
+                    chapter: 'Kitchen',
+                    box1: {
+                        title: `Let’s Get Solution for Building
+                        Construction Work`,
+                        image: `imgs/blog/tagKitchen1.png`,
+                        date: `26 December, 2022`,
+                        texts: [
+                            `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                            turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of
+                            Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or
+                            randomised words which don't look even slightly believable.`,
+                            `Embarrassing hidden in the middle of
+                            text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
+                            necessary.`
+                        ]
+                    },
+                    quote: `The details are not the details. They make the design.`,
+                    box2: {
+                        title: `Design sprints are great`,
+                        image: `imgs/blog/tagKitchen2.png`,
+                        texts: [
+                            `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                            turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem
+                            Ipsum available, but the majority have suffered.`,
+                            `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                            turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem
+                            Ipsum available, but the majority have suffered.`
+                        ],
+                        lists: [
+                            `Contrary to popular belief.There are many variations of passages of
+                            Lorem Ipsum available, but the majority have suffered.`,
+                            `Contrary to popular belief.There are many variations of passages of
+                            Lorem Ipsum available, but the majority have suffered.`,
+                            `Contrary to popular belief.There are many variations of passages of
+                            Lorem Ipsum available, but the majority have suffered.`
+                        ]
+                    }
+                },
+                {
+                    chapter: 'Bedroom',
+                    box1: {
+                        title: `The Bedroom: Your Restful Retreat`,
+                        image: `imgs/designs/bedroom1.jpeg`,
+                        date: `22 January, 2023`,
+                        texts: [
+                            `So much more than just where you sleep, today's bedrooms also serve as a home office, media room and a prime location for snuggling kids or pets. Bedrooms are also an excellent space for showing off your personal style. From Scandi to Farmhouse to Boho and every look in-between, our tips will help you make your bedroom a beautiful reflection of your style. Not sure if your look leans toward traditional or if cozy cottage is more your style — take our quiz, below, to find out.`,
+                            `With crisp white walls, wood beams and black accents, this spacious main suite definitely achieves a fresh, modern farmhouse aesthetic. Buffalo check pillows, a brown leather sofa and a white oriental rug reinforce this look, as well as pull in some interest and warmth. And, appropriately enough, a chic black barn door grants access to the main bathroom.`
+                        ]
+                    },
+                    quote: `Have no fear of perfection -- you’ll never reach it.`,
+                    box2: {
+                        title: `Modern Farmhouse-Style Suite`,
+                        image: `imgs/designs/bedroom2.webp`,
+                        texts: [
+                            `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                            turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem
+                            Ipsum available, but the majority have suffered.`,
+                            `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                            turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem
+                            Ipsum available, but the majority have suffered.`
+                        ]
+                    }
+                },
+                {
+                    chapter: 'Building',
+                    box1: {
+                        title: `Charming Gray Cape Cod With Backyard Built for Entertaining`,
+                        image: `imgs/designs/building1.webp`,
+                        date: `12 November, 2021`,
+                        texts: [
+                            `This lovely gray Cape Cod home features a guest house and backyard pool. The main home also has a high-end patio with outdoor kitchen, built-in fireplace and a TV for entertaining. Inside, a spacious farmhouse-style kitchen offers a large eat-in island, abundant storage and great amenities.`,
+                            `A living room with built-in shelves offers a great place to gather, and the master bedroom features a balcony with a lovely ocean view.`
+                        ]
+                    },
+                    quote: `Design is intelligence made visible.`,
+                    box2: {
+                        title: `Gray Cape Cod-Style Home`,
+                        image: `imgs/designs/building2.jpeg`,
+                        texts: [
+                            `A slate tile walkway leads up to a charming Cape Cod-style house with loads of charm.`,
+                            `Traditional gables and columns set the tone for the home's classic interiors, while a red front door beckons to guests and creates a fun focal point.`
+                        ]
+                    }
+                },
+                {
+                    chapter: 'Architecture',
+                    box1: {
+                        title: `About IS Architecture`,
+                        image: `imgs/designs/architecture1.jpeg`,
+                        date: `30 March, 2023`,
+                        texts: [
+                            `IS Architecture’s design philosophy is grounded on the principles of assessing a client’s needs and creating extraordinary spaces which compliment and maximize the site or existing structures.`,
+                            `Inherent in this philosophy is the combination of the highest quality product with exceptional client service. We approach each new project without preconceived notions: our designs are driven by each client’s needs, and our specialty is listening and responding with unique solutions.`
+                        ]
+                    },
+                    quote: `Everything is designed. Few things are designed well.`,
+                    box2: {
+                        title: `French Country Estate`,
+                        image: `imgs/designs/architecture2.jpeg`,
+                        texts: [
+                            `The main portion of this French country home and the fence enclosing the courtyard are built from natural stone.`,
+                            `The exterior of the private spaces of the home are clad in a contrasting stucco, drawing a visual boundary between public and private spaces, even from the exterior of the home.`
+                        ]
+                    }
+                },
+                {
+                    chapter: 'Kitchen Planning',
+                    box1: {
+                        title: `He Made Over His Childhood Kitchen, Now It's Cozy-Chic`,
+                        image: `imgs/designs/kitchenDesign1.jpeg`,
+                        date: `2 August, 2022`,
+                        texts: [
+                            `Love marble, love its imperfections. Alison considers the inevitable stains and etching part of the patina, and worth it for the pretty veining. As well, consider counter tricks.`,
+                            `And that’s not really a thick slab of marble on the island. It’s a countertop with mitered edges, a smart way to fake the look that involves joining together thinner pieces of stone. The waterfall edges on either side make it look even more luxe.`
+                        ]
+                    },
+                    quote: `You can't use up creativity. The more you use, the more you have.`,
+                    box2: {
+                        title: `An Outdoor Kitchen Dream Come True`,
+                        image: `imgs/designs/kitchenDesign2.jpeg`,
+                        texts: [
+                            `Architectural details are always a good thing. The coffered ceiling in the living room creates drama. Another way to add something extra: installing decorative beams.`,
+                            `Under-counter wine storage comes in handy. “It keeps your main fridge from getting too crowded, you can set it to the perfect temp, and it helps with resale value”.`
+                        ]
+                    }
+                },
+                {
+                    chapter: 'Bathroom',
+                    box1: {
+                        title: `31 Blue Bathroom Design Ideas`,
+                        image: `imgs/designs/bathroom1.jpeg`,
+                        date: `9 September, 2020`,
+                        texts: [
+                            `With less square footage to decorate or remodel, small bathrooms and powder rooms are ideal spaces to go all out on design. From fun flooring and fixture ideas to useful space-saving solutions, follow our expert tips and tricks to amp up and make the most out of your tiny space.`,
+                            `So, buy that quirky wallpaper you've been eyeing or install that cool mirror you thrifted to create a bathroom that's only small on space — not style.`
+                        ]
+                    },
+                    quote: `Design adds value faster than it adds costs.`,
+                    box2: {
+                        title: `Small Bathroom Solutions`,
+                        image: `imgs/designs/bathroom2.jpeg`,
+                        texts: [
+                            `Wet room bathrooms — a space fully built with materials designed to get wet — usually include a shower space and freestanding tub in their own glass enclosure. Wet rooms continue to be a smart solution for small spaces, but they're also turning up in luxury bathrooms. It's a natural step from the freestanding tub trend.`,
+                            `In this wet room, a stylish freestanding bathtub is strategically placed under an oversized picture window. Floors made of limestone complete the spa-like atmosphere.`
+                        ]
+                    }
+                }
+            ],
+            currentBlock:
+            {
+                chapter: 'Kitchen',
+                box1: {
+                    title: `Let’s Get Solution for Building
+                    Construction Work`,
+                    image: `imgs/blog/tagKitchen1.png`,
+                    date: `26 December, 2022`,
+                    texts: [
+                        `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                        turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of
+                        Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or
+                        randomised words which don't look even slightly believable.`,
+                        `Embarrassing hidden in the middle of
+                        text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
+                        necessary.`
+                    ]
+                },
+                quote: `The details are not the details. They make the design.`,
+                box2: {
+                    title: `Design sprints are great`,
+                    image: `imgs/blog/tagKitchen2.png`,
+                    texts: [
+                        `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                        turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem
+                        Ipsum available, but the majority have suffered.`,
+                        `Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                        turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem
+                        Ipsum available, but the majority have suffered.`
+                    ],
+                    lists: [
+                        `Contrary to popular belief.There are many variations of passages of
+                        Lorem Ipsum available, but the majority have suffered.`,
+                        `Contrary to popular belief.There are many variations of passages of
+                        Lorem Ipsum available, but the majority have suffered.`,
+                        `Contrary to popular belief.There are many variations of passages of
+                        Lorem Ipsum available, but the majority have suffered.`
+                    ]
+                }
+            },
+            tagsButtons:
+            {
+                title: `Tags`,
+                buttonNames: [`Kitchen`, `Bedroom`, `Building`, `Architecture`, `Kitchen Planning`, `Bathroom`]
+            }
+        }
+    },
+    methods: {
+        changeTag(buttonName) {
+            this.tagsData.forEach(element => {
+                if (element.chapter === buttonName) {
+                    this.currentBlock = element;
+                    console.log('changed');
+                }
+            });
+        }
+    },
+    template: `
+    <div class="details__content">
+        <div class="details__content_first">
+            <h1 class="details__title">{{ currentBlock.box1.title }}</h1>
+            <div class="details__subContent">
+                <img :src="currentBlock.box1.image" alt="photo of kitchen" class="details__image">
+                <div class="details__dateBreadcrumbsBox">
+                    <p class="details__date">{{currentBlock.box1.date}}</p>
+                    <p class="details__breadcrumbs">
+                        <a href="#" class="details__breadcrumb">Interior</a> /
+                        <a href="#" class="details__breadcrumb">Home</a> /
+                        <a href="#" class="details__breadcrumb">Decore</a>
+                    </p>
+                </div>
+            </div>
+            <div class="details__textBox">
+                <p class="details__text" v-for="text in currentBlock.box1.texts" :key="text.id">{{ text }}</p>
+            </div>
+        </div>
+        <div class="details__quotesBox">
+            <div class="details__quotes">
+                <p class="details__quote">“</p>
+                <p class="details__quoteText">{{currentBlock.quote}}</p>
+            </div>
+        </div>
+        <div class="details__content_second">
+            <h1 class="details__title">{{currentBlock.box2.title}}</h1>
+            <p class="details__text">{{currentBlock.box2.texts[0]}}</p>
+            <ol class="details__points">
+                <li class="details__point details__text" v-for="list in currentBlock.box2.lists" :key="list.id"><span class="blackColorText">{{list}}</span></li>
+            </ol>
+            <img :src="currentBlock.box2.image" alt="photo of kitchen" class="details__image details__image_withMargin">
+            <p class="details__text">{{currentBlock.box2.texts[1]}}</p>
+        </div>
+    </div>
+    <div class="tags">
+        <h3 class="tags__title">{{tagsButtons.title}}</h3>
+        <div class="tags__buttons">
+            <button class="tags__button" v-for="button in tagsButtons.buttonNames" :key="button.id" @click="changeTag(button)">{{ button }}</button>
+        </div>
+    </div>
+    `
+};
